4059150b3c9cd87f07759cea17deb84674c3ef12
leejuhi <anim0203@naver.com>
2025-03-17 16:00:44 +0900

    chore: 배포를 위한 조치
    타입규칙 걸리는거 주석 처리

--- src/components/CalendarDate.tsx
+++ src/components/CalendarDate.tsx
@@ -1,39 +1,39 @@
-import styled from "styled-components";
-import { loadData, TodoDataInfo } from "../utils/storage";
+// import styled from "styled-components";
+// import { loadData, TodoDataInfo } from "../utils/storage";
 
-interface CalendarDateProps {
-  index: number;
-  date: string;
-  condition: boolean;
-}
+// interface CalendarDateProps {
+//   index: number;
+//   date: string;
+//   condition: boolean;
+// }
 
-const CalendarDate = ({ date, condition }: CalendarDateProps) => {
-  const todoData: TodoDataInfo | [] = loadData();
+// const CalendarDate = ({ date, condition }: CalendarDateProps) => {
+//   const todoData: TodoDataInfo | [] = loadData();
 
-  return (
-    <DateContainer condition={condition}>
-      {date}
-      <TodoCount></TodoCount>
-    </DateContainer>
-  );
-};
-export default CalendarDate;
-const DateContainer = styled.div<{ condition: boolean }>`
-  box-sizing: border-box;
-  width: calc(100% / 7);
-  height: 50px;
-  border: 0.5px solid #000;
-  &:nth-child(7n + 1) {
-    color: red;
-  }
-  &:nth-child(7n) {
-    color: rgb(68, 0, 255);
-  }
-  ${(props) => (props.condition ? `color:pink !important;` : ``)}
-`;
-const TodoCount = styled.div`
-  font-size: 10px;
-  color: gray;
-  text-align: right;
-  margin-top: 5px;
-`;
+//   return (
+//     <DateContainer condition={condition}>
+//       {date}
+//       <TodoCount></TodoCount>
+//     </DateContainer>
+//   );
+// };
+// export default CalendarDate;
+// const DateContainer = styled.div<{ condition: boolean }>`
+//   box-sizing: border-box;
+//   width: calc(100% / 7);
+//   height: 50px;
+//   border: 0.5px solid #000;
+//   &:nth-child(7n + 1) {
+//     color: red;
+//   }
+//   &:nth-child(7n) {
+//     color: rgb(68, 0, 255);
+//   }
+//   ${(props) => (props.condition ? `color:pink !important;` : ``)}
+// `;
+// const TodoCount = styled.div`
+//   font-size: 10px;
+//   color: gray;
+//   text-align: right;
+//   margin-top: 5px;
+// `;
