ee0ee7f5a0e3b18556538a70936b7d6c42bb3bb4
young <azerosong@gmail.com>
2025-03-18 20:22:05 +0900

    fix: useCallback 중첩으로 삭제 버튼 클릭 시 allTodos가 제대로 업데이트되지 않는 버그 수정

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,10 +1,8 @@
-import { useCallback } from 'react';
 import type { TodoItemProps } from './dto';
 import { Button, Checkbox, TodoContent, TodoItemLayout } from './style';
 
 export default function TodoItem({ id, isDone, content, onChange, onClickDeleteButton }: TodoItemProps) {
 	const todoId = String(id);
-	const handleDeleteButtonClick = useCallback(() => onClickDeleteButton(todoId), []);
 
 	return (
 		<TodoItemLayout>
@@ -12,7 +10,7 @@ export default function TodoItem({ id, isDone, content, onChange, onClickDeleteB
 			<TodoContent htmlFor={todoId} $isDone={isDone}>
 				{content}
 			</TodoContent>
-			<Button onClick={handleDeleteButtonClick}>삭제</Button>
+			<Button onClick={() => onClickDeleteButton(todoId)}>삭제</Button>
 		</TodoItemLayout>
 	);
 }
