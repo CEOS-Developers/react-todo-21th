720342f92c5ad18fc83f0561fb7052b0e18e6341
DefineXX <keaikim21@naver.com>
2025-03-19 01:53:02 +0900

    chore: styled-components 설치 및 스타일 가이드 세팅

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+type Colors = {
+  [key: string]: string;
+};
+
+const colors: Colors = {
+  almond: '#EAE0D5',
+  beige: '#C6AC8F',
+  walnut: '#5E503F',
+};
+
+export default colors;
---
+++ src/styles/typography.ts
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+
+const fontStyles = {
+  Header1: css``,
+  Header2: css``,
+  Body1: css``,
+  Body2: css``,
+  Caption1: css``,
+  Caption2: css``,
+};
+
+export default fontStyles;
