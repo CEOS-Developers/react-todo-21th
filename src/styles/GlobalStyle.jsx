1736a9414cfeeb97b0f274b96352b14568c27dc4
Yeongseo Kim <1903020@ewhain.net>
2025-03-19 14:31:53 +0900

    style: pretendard 폰트 적용

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -4,6 +4,7 @@ const GlobalStyle = createGlobalStyle/*css */ `
 body, html{
     height: 100%;
     overflow: hidden;
+    font-family:  ${(props) => props.theme.fonts.main};
 }
 body{
     background-color: ${(props) => props.theme.colors.lightest};
