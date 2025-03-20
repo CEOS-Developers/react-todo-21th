009905b945faead60fde15a67c77c30a405f2703
Yeongseo Kim <1903020@ewhain.net>
2025-03-20 17:05:31 +0900

    style: Header font 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const HeaderTitle = styled.h1`
   color: ${(props) => props.theme.colors.darkest};
   margin: 10px 10px;
   display: inline-block;
+  font-family: ${(props) => props.theme.fonts.heading};
 `
 
 const Header = () => {
