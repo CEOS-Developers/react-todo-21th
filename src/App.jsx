b21b8ee9978d7c051a894b31879c90eef8620538
Yeongseo Kim <1903020@ewhain.net>
2025-03-20 13:23:57 +0900

    feat: todo, pivotDate 상태 추가 및 Create, Update, Delete 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,103 @@ import { ThemeProvider } from 'styled-components'
 import './App.css'
 import { theme } from './utils/theme'
 import GlobalStyle from './styles/GlobalStyle'
+import { useReducer, useState } from 'react'
+import { createContext } from 'react'
+import formatDate from './utils/formatDate'
+
+const mockData = {
+  '2025-03-17': [
+    { id: '1742276032173', content: '2', isFinished: false },
+    { id: '1742276032462', content: '3', isFinished: false },
+    { id: '1742276032956', content: '4', isFinished: false },
+    { id: '1742276031891', content: '1', isFinished: true },
+  ],
+  '2025-03-18': [
+    { id: '1742276032173', content: '2', isFinished: false },
+    { id: '1742276032462', content: '3', isFinished: false },
+    { id: '1742276032956', content: '4', isFinished: false },
+    { id: '1742276031891', content: '1', isFinished: true },
+  ],
+  '2025-03-20': [
+    { id: '1742284939484', content: '세오스 엠티', isFinished: false },
+    { id: '1742362973445', content: '1', isFinished: false },
+  ],
+}
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'CREATE':
+      return {
+        ...state,
+        [action.date]: [
+          ...state[action.date],
+          {
+            id: action.data.id,
+            content: action.data.content,
+            isFinished: false,
+          },
+        ],
+      }
+    case 'TOGGLE_STATUS':
+      return {
+        ...state,
+        [action.date]: state[action.date].map((todo) =>
+          todo.id === action.id ? { ...todo, isFinished: !todo.isFinished } : todo
+        ),
+      }
+    case 'DELETE':
+      return {
+        ...state,
+        [action.date]: state[action.date].filter((todo) => todo.id !== action.id),
+      }
+    default:
+      return state
+  }
+}
+
+export const TodoStateContext = createContext()
+export const TodoDispatchContext = createContext()
 
 function App() {
+  const [todos, updateTodos] = useReducer(todoReducer, mockData)
+  const [pivotDate, setPivotDate] = useState(formatDate(new Date()))
+
+  const onCreate = (id, content) => {
+    updateTodos({
+      type: 'CREATE',
+      date: pivotDate,
+      data: {
+        id,
+        content,
+      },
+    })
+  }
+
+  const onUpdateStatus = (id) => {
+    updateTodos({
+      type: 'TOGGLE_STATUS',
+      date: pivotDate,
+      id,
+    })
+  }
+
+  const onDelete = (id) => {
+    updateTodos({
+      type: 'DELETE',
+      date: pivotDate,
+      id,
+    })
+  }
+
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <header>
-          <h1>Todo</h1>
-        </header>
-      </ThemeProvider>
+      <TodoStateContext.Provider value={{ todos, pivotDate }}>
+        <TodoDispatchContext.Provider value={{ onCreate, onUpdateStatus, onDelete, setPivotDate }}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+          </ThemeProvider>
+        </TodoDispatchContext.Provider>
+      </TodoStateContext.Provider>
     </>
   )
 }
