8f4646b679b14aca6e4a92c889a9cf3771e2d6fc
chaeyoungwon <epopcy@naver.com>
2025-03-20 15:27:24 +0900

    Environment: 초기 환경 세팅

---
+++ react-todo/src/styles/GlobalStyles.ts
@@ -0,0 +1,71 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+
+    :root  {
+        --vh: 100%;
+    }
+
+    *{
+        box-sizing: border-box;
+    }
+
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 10px;
+        vertical-align: baseline;
+    }
+
+    html {
+        font-size: 16px; 
+        @media (min-width: 450px) and (max-width: 600px) {
+            font-size: 13px;
+        }
+        @media (max-width: 450px) {
+            font-size: 10px;
+        }
+    }
+
+    body{
+        display: flex; 
+        flex-direction: column;
+        justify-content: center;
+
+        font-family: 'Pretendard';
+        line-height: 1;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        
+        &::-webkit-scrollbar {
+            display: none; /* 웹 브라우저에서 스크롤 바 숨기기 */
+        }
+    }
+
+
+    ol, ul{
+        list-style: none;
+    }
+    
+    a{
+        text-decoration: none;
+        color: inherit;
+        cursor: pointer;
+    }
+
+    button {
+        border: 0;
+        background: transparent;
+        cursor: pointer;
+
+        &:disabled {
+            pointer-events: none;
+        }
+    }
+`;
+
+export default GlobalStyles;
