941186270b2425dbc1773e546e753f12844982c8
seoyeon5117 <seoyun020207@gmail.com>
2025-03-20 15:42:14 +0900

    feat: 초기 설정

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "../styles/GlobalStyles";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
