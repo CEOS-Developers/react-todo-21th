8725d6f49adbda545e72f50b1711918f1aea37b7
Yeongseo Kim <1903020@ewhain.net>
2025-03-20 16:07:21 +0900

    DailyContentHeader 컴포넌트에 날짜 선택 기능 구현

--- src/components/DailyContentHeader.jsx
+++ src/components/DailyContentHeader.jsx
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react'
-import { HeadTitle, SubTitle } from '../styles/Title'
+import React, { useContext, useRef } from 'react'
+import { TodoDispatchContext, TodoStateContext } from '../App'
 import { useTheme } from 'styled-components'
-import { TodoStateContext } from '../App'
+
+import { HeadTitle, SubTitle } from '../styles/Title'
 import FlexContainerRow from '../styles/FlexContainerRow'
+
 import getFinishedTodoCount from '../utils/getFinishedTodoCount'
 
 const DailyContentHeader = () => {
@@ -12,18 +14,33 @@ const DailyContentHeader = () => {
   const dateObj = new Date(pivotDate)
   const [day, month, date, year] = dateObj.toString().split(' ')
 
-  const dailyTodos = todos[pivotDate]
+  const dailyTodos = todos[pivotDate] || []
   const finishedTodosCount = getFinishedTodoCount(dailyTodos)
 
+  const dateInputRef = useRef(null)
+  const onClickDate = () => {
+    dateInputRef.current.showPicker()
+  }
+  const { setPivotDate } = useContext(TodoDispatchContext)
+  const onChangeDate = (e) => {
+    setPivotDate(e.target.value)
+  }
   return (
     <div>
-      <SubTitle>{year}</SubTitle>
+      <input ref={dateInputRef} onChange={onChangeDate} type="date" hidden />
+      <SubTitle clickable="true" onClick={onClickDate}>
+        {year}
+      </SubTitle>
       <FlexContainerRow>
-        <HeadTitle>{`${month} ${date} ${day}`}</HeadTitle>
+        <HeadTitle clickable="true" onClick={onClickDate}>
+          {`${month} ${date} ${day}`}
+        </HeadTitle>
         <HeadTitle
           style={{
             color: theme.colors.point,
-          }}>{`${finishedTodosCount}/${dailyTodos.length}`}</HeadTitle>
+          }}>
+          {`${finishedTodosCount}/${dailyTodos.length}`}
+        </HeadTitle>
       </FlexContainerRow>
     </div>
   )
