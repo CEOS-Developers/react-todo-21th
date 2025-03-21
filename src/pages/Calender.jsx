3c0ae108751e493ade18879f15c908b3ab98a164
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-03-22 02:11:15 +0900

    test: 날짜관련 테스트 코드 삭제

--- src/pages/Calender.jsx
+++ src/pages/Calender.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { formatDate } from '../utils/dateFormatter';
 
 function Calender(){
   const [date, setDate] = useState(new Date());
@@ -10,20 +11,19 @@ function Calender(){
 
   const handleDateChange = (selectedDate) => {
     setDate(selectedDate);
-    const formattedDate = selectedDate.toISOString().split("T")[0]; // YYYY-MM-DD 형식
-    navigate(`/dailyTodo/${formattedDate}`); // 페이지 이동
+    const formattedDate = formatDate(selectedDate);
+    navigate(`/dailyTodo/${formattedDate}`);
   };
   
   return (
     <div>
-      <Header title="MonthlyTodoList" />
+      <Header title="Canlender" />
       <p>날짜를 클릭해서 할 일을 등록해 보세요</p>
       <section className="calendarSection">
         <Calendar
           onChange={handleDateChange} // 날짜 변경 시 상태 업데이트
           value={date} // 현재 선택된 날짜
         />
-        <p>선택한 날짜: {date.toLocaleDateString()}</p>
       </section>
     </div>
   );
