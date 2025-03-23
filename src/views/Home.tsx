267df31062b7e16eca7b11eba692af9b476775be
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-03-23 20:58:16 +0900

    feat: add home view

---
+++ src/views/Home.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import { useCalendarLogic } from '../hooks/calendar/useCalendarLogic';
+import { useDateTodos } from '../hooks/todos/useDateTodos';
+import Calendar from '../components/Calendar';
+import TodoInput from '../components/TodoInput';
+import TodoList from '../components/TodoList';
+
+function Home() {
+  const calendarLogic = useCalendarLogic();
+  const dateTodos = useDateTodos();
+
+  return (
+    <Container>
+      <Calendar
+        today={calendarLogic.today}
+        selectedDateKey={calendarLogic.selectedDateKey}
+        thisYear={calendarLogic.calendarDate.getFullYear()}
+        thisMonth={calendarLogic.calendarDate.getMonth()}
+        daysByWeek={calendarLogic.daysByWeek}
+        onChangeMonth={calendarLogic.changeMonth}
+        onGoToToday={calendarLogic.goToToday}
+        onSelectDateKey={calendarLogic.selectDateKey}
+      />
+      <TodoInput
+        selectedDateKey={calendarLogic.selectedDateKey}
+        onAddTodo={dateTodos.addTodo}
+      />
+      <TodoList
+        dateKey={calendarLogic.selectedDateKey}
+        todos={dateTodos.dateTodos[calendarLogic.selectedDateKey]}
+        onToggleTodo={dateTodos.toggleTodo}
+        onRemoveTodo={dateTodos.removeTodo}
+      />
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div``;
